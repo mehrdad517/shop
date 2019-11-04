cfdd595a5ba82ebed2958a25bf226c0743fe6cff
mehrdad517 <mehrdad.masoumi517@gmail.com>
2019-11-04 17:46:15 +0330

    ls

--- backend/app/Order.php
+++ backend/app/Order.php
@@ -6,10 +6,104 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-
     protected $table = 'order';
 
     protected $primaryKey = 'id';
+
+    const ORDER_STATUS_PENDING = 0;
+    const ORDER_STATUS_ACCEPTED = 1;
+    const ORDER_STATUS_REJECTED = 2;
+    const ORDER_STATUS_RETURNED = 3;
+
+    const TRANSPORT_STATUS_DEFAULT = 0;
+    const TRANSPORT_STATUS_EXIT_THE_STOREROOM = 1;
+    const TRANSPORT_STATUS_IN_QUEUE = 2;
+    const TRANSPORT_STATUS_RESEND = 3;
+
+    const DELIVERY_STATUS_DEFAULT=0;
+    const DELIVERY_STATUS_ACCEPTED=1;
+    const DELIVERY_STATUS_POST_OFFICE =2;
+    const DELIVERY_STATUS_RETURN_TO_STOREROOM =3;
+
+    const ITEMS_STATUS_DEFAULT = 0;
+    const ITEMS_STATUS_FRACTIONAL = 1;
+    const ITEMS_STATUS_DEFECTIVE =2;
+
+    public function status($key= null)
+    {
+        $list = [
+            self::ORDER_STATUS_PENDING  => 'در انتظار',
+            self::ORDER_STATUS_ACCEPTED => 'تایید شده',
+            self::ORDER_STATUS_REJECTED => 'لغو شده',
+            self::ORDER_STATUS_RETURNED => 'مرجوعی',
+        ];
+        if ($key) {
+            if (isset($list[$key])) {
+                return $list[$key];
+            } else {
+                return 'نامشخص';
+            }
+        } else {
+            return $list;
+        }
+    }
+
+    public function transport($key= null)
+    {
+        $list = [
+            self::TRANSPORT_STATUS_DEFAULT => 'پیش فرض',
+            self::TRANSPORT_STATUS_EXIT_THE_STOREROOM => 'خروج از انبار',
+            self::TRANSPORT_STATUS_IN_QUEUE => 'در صف ارسال',
+            self::TRANSPORT_STATUS_RESEND => 'در صف ارسال مجدد',
+        ];
+        if ($key) {
+            if (isset($list[$key])) {
+                return $list[$key];
+            } else {
+                return 'نامشخص';
+            }
+        } else {
+            return $list;
+        }
+    }
+
+    public function delivery($key= null)
+    {
+        $list = [
+            self::DELIVERY_STATUS_DEFAULT => 'پیش فرض',
+            self::DELIVERY_STATUS_ACCEPTED => 'تحویل مشتری',
+            self::DELIVERY_STATUS_POST_OFFICE  => 'تحویل پست',
+            self::DELIVERY_STATUS_RETURN_TO_STOREROOM  => 'بازگشت به انبار',
+        ];
+        if ($key) {
+            if (isset($list[$key])) {
+                return $list[$key];
+            } else {
+                return 'نامشخص';
+            }
+        } else {
+            return $list;
+        }
+    }
+
+    public function items($key = null)
+    {
+        $list = [
+            self::ITEMS_STATUS_DEFAULT =>  'پیش فرض',
+            self::ITEMS_STATUS_FRACTIONAL =>  'کسری کالا',
+            self::ITEMS_STATUS_DEFECTIVE => 'معیوبی کالا',
+        ];
+        if ($key) {
+            if (isset($list[$key])) {
+                return $list[$key];
+            } else {
+                return 'نامشخص';
+            }
+        } else {
+            return $list;
+        }
+    }
+
 
     public function productPins()
     {
--- backend/database/migrations/2019_11_02_140840_create_order_table.php
+++ backend/database/migrations/2019_11_02_140840_create_order_table.php
@@ -16,14 +16,16 @@ class CreateOrderTable extends Migration
         Schema::create('order', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('user_id')->index();
-            $table->integer('tax_id');
+            $table->integer('tax_id', 1);
             $table->decimal('post_cost', 18, 2)->default(0);
             $table->decimal('tax', 18, 2)->default(0);
             $table->decimal('discount', 18, 2)->default(0);
             $table->decimal('pure_price', 18, 2);
             $table->decimal('total_price', 18, 2);
-            $table->boolean('status')->default(0);
-            $table->tinyInteger('transport')->default(0);
+            $table->smallInteger('order_status')->default(0);
+            $table->smallInteger('transport_status')->default(0);
+            $table->smallInteger('delivery_status')->default(0);
+            $table->smallInteger('items_status')->default(0);
             $table->timestamps();
         });
     }
