7fc252d01bb7774df617df8d22f8e9a518b4ddc4
mehrdad517 <mehrdad.masoumi517@gmail.com>
2020-03-03 00:41:11 +0330

    ls

--- frontend/src/app/index.js
+++ frontend/src/app/index.js
@@ -19,27 +19,27 @@ import './styles.scss';
 
 const theme = createMuiTheme({
   direction: "rtl",
-  palette: {
-    primary: {
-      // light: will be calculated from palette.primary.main,
-      main: '#ff934c',
-      // dark: will be calculated from palette.primary.main,
-      // contrastText: will be calculated to contrast with palette.primary.main
-    },
-    secondary: {
-      light: '#0066ff',
-      main: '#0044ff',
-      // dark: will be calculated from palette.secondary.main,
-      contrastText: '#fff',
-    },
-    // Used by `getContrastText()` to maximize the contrast between
-    // the background and the text.
-    contrastThreshold: 3,
-    // Used by the functions below to shift a color's luminance by approximately
-    // two indexes within its tonal palette.
-    // E.g., shift from Red 500 to Red 300 or Red 700.
-    tonalOffset: 0.2,
-  },
+  // palette: {
+  //   primary: {
+  //     // light: will be calculated from palette.primary.main,
+  //     main: '#ff934c',
+  //     // dark: will be calculated from palette.primary.main,
+  //     // contrastText: will be calculated to contrast with palette.primary.main
+  //   },
+  //   secondary: {
+  //     light: '#0066ff',
+  //     main: '#0044ff',
+  //     // dark: will be calculated from palette.secondary.main,
+  //     contrastText: '#fff',
+  //   },
+  //   // Used by `getContrastText()` to maximize the contrast between
+  //   // the background and the text.
+  //   contrastThreshold: 3,
+  //   // Used by the functions below to shift a color's luminance by approximately
+  //   // two indexes within its tonal palette.
+  //   // E.g., shift from Red 500 to Red 300 or Red 700.
+  //   tonalOffset: 0.2,
+  // },
   overrides: {
     MuiListItem: {
       root: {
@@ -84,8 +84,12 @@ const theme = createMuiTheme({
       },
     },
     MuiInputBase: {
+      root: {
+        fontFamily: 'Shabnam !important',
+      },
       input: {
-        fontSize: '14px'
+        fontSize: '14px',
+        font: 'Shabnam !important',
       },
     },
     MuiFormHelperText: {
--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Container, Input, Paper, TextField} from "@material-ui/core";
+import {Container, Input, Paper, TextField, Grid} from "@material-ui/core";
 import style from './header.scss'
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -9,6 +9,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Slide from '@material-ui/core/Slide';
+import Typography from '@material-ui/core/Typography';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -24,33 +25,99 @@ class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      open : false,
+      open: false,
+      username: [0, 9],
+      code: [],
+      verify: false,
+      sending: false,
+      counter: 60
     }
   }
 
   componentDidMount() {
 
-    for (let i = 0; i<= 10; i++) {
+    for (let i = 0; i<= 8; i++) {
+      if (i <= 4) {
+        this[`code_${i}`] = React.createRef();
+      }
       this[`username_${i}`] = React.createRef();
     }
 
   }
 
 
-  handleChangeUsername(event, index) {
+  handleChangeUsername(event, index, operator) {
 
-    if (typeof this[`username_${index + 1}`] != "undefined") {
-      this[`username_${index + 1}`].current.disabled = false;
-      this[`username_${index + 1}`].current.focus();
+    if (operator === 'username') {
+
+      let username = this.state.username;
+
+      if (typeof this[`username_${index + 1}`] != "undefined") {
+
+        this[`username_${index + 1}`].current.disabled = false;
+        this[`username_${index + 1}`].current.focus();
+
+        username.push(parseInt(event.target.value));
+
+      } else {
+
+        username.push(parseInt(event.target.value));
+        // send request and show verify modal
+        this.setState({
+          sending: true,
+          username
+        });
+
+        let interval = setInterval(() => {
+          this.setState({
+            counter: this.state.counter - 1
+          });
+
+          if (this.state.counter === 0) {
+            this.setState({
+              username: [0, 9],
+              code: [],
+              verify: false,
+              sending: false,
+              counter: 60
+            });
+            clearInterval(interval);
+          }
+        }, 1000)
+
+
+        // send to api
+      }
     } else {
-      // send request and show verify modal
-    console.log('xxxx')
+
+      let code = this.state.code;
+
+      if (typeof this[`code_${index + 1}`] != "undefined") {
+
+        this[`code_${index + 1}`].current.disabled = false;
+        this[`code_${index + 1}`].current.focus();
+
+        code.push(parseInt(event.target.value));
+
+      } else {
+        code.push(parseInt(event.target.value)); // last record
+        // send request and show verify modal
+        this.setState({
+          verify: true,
+          open: false,
+          code
+        });
+
+
+        // send to api
+      }
+    }
   }
 
-  }
 
 
   render() {
+    console.log(this.state);
     return (
       <div>
         <Paper className={style.header}>
@@ -63,24 +130,46 @@ class Header extends Component {
               open={this.state.open}
               TransitionComponent={Transition}
               keepMounted
-              aria-labelledby="alert-dialog-slide-title"
-              aria-describedby="alert-dialog-slide-description"
             >
-              <DialogTitle id="alert-dialog-slide-title">{"ثبت نام"}</DialogTitle>
+              <div className={style.wrapper}>
+              <DialogTitle style={{ textAlign: "center"}}>
+                {this.state.sending === true ? 'تایید حساب کاربری' : 'ورود به سایت'}
+              </DialogTitle>
               <DialogContent>
                 <div className={style.login}>
-                  {[0,1,2,3,4,5,6,7,8,9, 10].map((value, key) => {
-                    let pattern;
-                    if (key === 0) pattern = 0; else if (key === 1) pattern = 9; else if(key === 2) pattern = 1; else if (key === 3) pattern = 2; else pattern = 0;
-                    return(<TextField disabled={key === 0 ? false : true} key={key} placeholder={`${pattern}`} variant={"outlined"}  inputRef={this[`username_${key}`]} onChange={(event) => this.handleChangeUsername(event, key)} />);
-                  })}
+                  {this.state.sending === false ?
+                    <>
+                    <TextField value={0} disabled={true} variant={"outlined"} />
+                    <TextField value={9} disabled={true} variant={"outlined"} />
+                    {[0,1,2,3,4,5,6,7,8].map((value, key) => {
+                      let pattern;
+                      if(key === 0) pattern = 1; else if (key === 1) pattern = 2; else pattern = 0;
+                      return(<TextField
+                          disabled={key === 0 ? false : true} key={key}
+                          placeholder={`${pattern}`}
+                          variant={"outlined"}
+                          inputRef={this[`username_${value}`]}
+                          onChange={(event) => this.handleChangeUsername(event, key, 'username')}
+                        />
+                      );
+                    })}
+                  </> : <>
+                    {[0,1, 2,3,4].map((value, key) => {
+                      return(<TextField
+                          disabled={key === 0 ? false : true}
+                          key={key}
+                          placeholder={'*'}
+                          variant={"outlined"}
+                          inputRef={this[`code_${value}`]}
+                          onChange={(event) => this.handleChangeUsername(event, key, 'code')}
+                        />
+                      );
+                    })}
+                  </>}
                 </div>
+                {this.state.sending === true && this.state.counter > 0 && <Typography variant={"p"}>{ 'کد دریافتی را وارد نمایید' + ' (' + this.state.counter + ')'}</Typography>}
               </DialogContent>
-              <DialogActions>
-                <Button onClick={() =>this.setState({ open : false})} color="primary">
-                  Disagree
-                </Button>
-              </DialogActions>
+              </div>
             </Dialog>
           </Container>
         </Paper>
